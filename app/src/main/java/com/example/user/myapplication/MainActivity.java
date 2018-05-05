bd180f1500701f6800ad2205a62fa5da8236df73
Chow <travisckelly@yahoo.com>
2018-05-06 00:28:26 +0800

    added Gif to button weather and typhoon

--- app/src/main/java/com/example/user/myapplication/MainActivity.java
+++ app/src/main/java/com/example/user/myapplication/MainActivity.java
@@ -1,13 +1,34 @@
 package com.example.user.myapplication;
 
+import android.graphics.drawable.AnimationDrawable;
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    AnimationDrawable weatherGif;
+    AnimationDrawable typhoonGif;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageButton weatherImage = (ImageButton) findViewById(R.id.weatherbtn);
+        ImageButton typhoonImage = (ImageButton) findViewById(R.id.typhoonbtn);
+        weatherImage.setBackgroundResource(R.drawable.rain);
+        typhoonImage.setBackgroundResource(R.drawable.typhoon);
+        weatherGif = (AnimationDrawable) weatherImage.getBackground();
+        typhoonGif = (AnimationDrawable) typhoonImage.getBackground();
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        weatherGif.start();
+        typhoonGif.start();
     }
 }
