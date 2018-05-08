5fca0f0d880bd7ccb1879e6a2f2aee9e80a29aac
Chow <travisckelly@yahoo.com>
2018-05-08 21:38:01 +0800

    Updated Gifs Link buttons to added classes can show any two gifs adding third causes program to crash

--- app/src/main/java/com/example/user/myapplication/MainActivity.java
+++ app/src/main/java/com/example/user/myapplication/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.user.myapplication;
 
+import android.content.Intent;
 import android.graphics.drawable.AnimationDrawable;
 import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 
@@ -11,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
 
     AnimationDrawable weatherGif;
     AnimationDrawable typhoonGif;
+    AnimationDrawable earthquakeGif;
 
 
     @Override
@@ -19,16 +22,38 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         ImageButton weatherImage = (ImageButton) findViewById(R.id.weatherbtn);
-        ImageButton typhoonImage = (ImageButton) findViewById(R.id.typhoonbtn);
         weatherImage.setBackgroundResource(R.drawable.rain);
-        typhoonImage.setBackgroundResource(R.drawable.typhoon);
         weatherGif = (AnimationDrawable) weatherImage.getBackground();
-        typhoonGif = (AnimationDrawable) typhoonImage.getBackground();
+
+//        ImageButton typhoonImage = (ImageButton) findViewById(R.id.typhoonbtn);
+//        typhoonImage.setBackgroundResource(R.drawable.typhoon);
+//        typhoonGif = (AnimationDrawable) typhoonImage.getBackground();
+
+        ImageButton earthQuake = (ImageButton)findViewById(R.id.earthquakebtn);
+        earthQuake.setBackgroundResource(R.drawable.earthquake);
+        earthquakeGif = (AnimationDrawable) earthQuake.getBackground();
+
     }
     @Override
     protected void onStart() {
         super.onStart();
         weatherGif.start();
-        typhoonGif.start();
+        //typhoonGif.start();
+        earthquakeGif.start();
+
     }
+    public void WeekWeather(View view){
+        Intent intent = new Intent(this, WeekWeather.class);
+        startActivity(intent);
+    }
+    public  void TyphoonWarnings(View view){
+        Intent intent = new Intent(this, TyphoonWarnings.class);
+        startActivity(intent);
+    }
+    public void EarthquakeWarnings(View view){
+        Intent intent = new Intent(this, EarthquakeWarnings.class);
+        startActivity(intent);
+    }
+
+
 }
